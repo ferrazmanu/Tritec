4a4e85ec00f2810e35a31cc2ff5ca5bd7f8a6ea1
ferrazmanu <ferraz.manuela@hotmail.com>
2025-05-25 17:22:17 -0300

    feat: post de formulário e get de head e depoimentos

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
-// FORM LOGIC
-const telefoneInput = document.getElementById("telefone");
+const API_URL = "https://api-tritec.criativocosmos.com";
+const IMAGE_URL = "https://painel-tritec.criativocosmos.com/uploads";
 
-const telefoneMask = IMask(telefoneInput, {
+// FORM
+const phoneInput = document.getElementById("telefone");
+
+const phoneMask = IMask(phoneInput, {
   mask: ["(00) 0000-0000", "(00) 00000-0000"],
 });
 
@@ -13,34 +16,47 @@ const CNPJInputMask = IMask(CNPJInput, {
 
 const form = document.querySelector(".contact-form");
 
-form.addEventListener("submit", function (e) {
+form.addEventListener("submit", async (e) => {
   e.preventDefault();
 
-  const cleanCNPJValue = cnpjInput.masked.unmaskedValue;
+  const cleanCNPJValue = CNPJInputMask.unmaskedValue;
+  const cleanPhoneValue = phoneMask.unmaskedValue;
 
-  const nome = form.nome.value;
-  const empresa = form.empresa.value;
-  const cnpj = form.cnpj.value;
-  const cidade = form.cidade.value;
-  const telefone = form.telefone.value;
-  const email = form.email.value;
+  const body = {
+    name: document.getElementById("nome").value,
+    email: document.getElementById("email").value,
+    phone: cleanPhoneValue,
+    subject: "Cadastro Revendedor Tritec",
+    estado: document.getElementById("cidade").value.split("/")[1]?.trim() || "",
+    cnpj: cleanCNPJValue,
+    nomeEmpresa: document.getElementById("empresa").value,
+    cidade: document.getElementById("cidade").value.split("/")[0]?.trim() || "",
+  };
 
-  console.log({ nome, empresa, cnpj, cidade, telefone, email });
+  try {
+    const response = await fetch(`${API_URL}/form`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
 
-  alert("Formulário enviado com sucesso!");
-  form.reset();
+    if (response.ok) {
+      showToast(
+        "Solicitação enviada com sucesso! Em breve entraremos em contato!"
+      );
+      form.reset();
+    } else {
+      showToast("Erro ao enviar. Tente novamente.");
+    }
+  } catch (error) {
+    console.error("Erro na requisição:", error);
+    alert("Erro de conexão. Verifique sua internet ou tente mais tarde.");
+  }
 });
 
-// CAROUSEL LOGIC
-const swiper = new Swiper(".swiper", {
-  loop: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
-
-// ANIMATION LOGIC
+// ANIMATION
 function setupAnimationObserver(
   triggerClass,
   animationClass,
@@ -61,3 +77,91 @@ function setupAnimationObserver(
 setupAnimationObserver("animate-from-left", "animation-from-left-start");
 setupAnimationObserver("animate-from-bottom", "animation-from-bottom-start");
 setupAnimationObserver("animate-open", "animation-open");
+
+// FETCH INFO
+
+async function fetchInfo() {
+  try {
+    const response = await fetch(`${API_URL}/pages/home`);
+    const data = await response.json();
+
+    //HEAD INFO
+    const headData = data.head;
+
+    document.title = `Tritec | ${headData.pageTitle}`;
+
+    const metaDescription = document.createElement("meta");
+    metaDescription.name = "description";
+    metaDescription.content = headData.metaDescription;
+    document.head.appendChild(metaDescription);
+
+    if (headData.imageOpenGraph) {
+      const metaOG = document.createElement("meta");
+      metaOG.setAttribute("property", "og:image");
+      metaOG.content = headData.imageOpenGraph;
+      document.head.appendChild(metaOG);
+    }
+
+    // if (headData.headScripts) {
+    //   const script = document.createElement("script");
+    //   script.text = headData.headScripts;
+    //   document.head.appendChild(script);
+    // }
+
+    //TESTIMONIALS INFO
+    const testimonials = data.depoimentos;
+    const wrapper = document.getElementById("testimonials-wrapper");
+
+    testimonials.forEach((item) => {
+      const slide = document.createElement("div");
+      slide.classList.add("swiper-slide");
+
+      const imgSrc = `${IMAGE_URL}/${item.imagem}`;
+
+      slide.innerHTML = `
+        <div class="testimonial-box">
+          <div class="title">
+            <div class="image">
+              <img src="${imgSrc}" alt="${item.titulo}" />
+            </div>
+            <div class="name">
+              <span>${item.titulo}</span>
+            </div>
+          </div>
+          <div class="testimonial-text">
+            ${item.descricao}
+          </div>
+        </div>
+      `;
+
+      wrapper.appendChild(slide);
+    });
+
+    new Swiper(".swiper", {
+      loop: true,
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  } catch (error) {
+    console.error("Erro ao buscar dados do head:", error);
+  }
+}
+
+fetchInfo();
+
+// TOAST
+function showToast(message) {
+  const toast = document.getElementById("toast");
+  toast.textContent = message;
+  toast.className = "toast show";
+
+  setTimeout(() => {
+    toast.className = toast.className.replace("show", "");
+  }, 3000);
+}
